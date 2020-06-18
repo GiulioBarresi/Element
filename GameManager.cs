906f054d1c60063e5c36934f799fba385b5161ea
Giulio Barresi <>
2020-06-18 17:39:37 +0200

    scripts

---
+++ GameManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    static public int count;
+    public static PlayerInteraction playerInteraction;
+    public static InteractiveElement interactiveElement;
+    public static FireOnBrazier fireOnBrazier;
+    public static Health health;
+    public static AudioManager audioManager;
+    public static Door2 door2;
+    public static Damage damage;
+    public static PlayerController4 playerController4;
+
+    public GameObject playerGameObject;
+
+    public GameObject FakePlayer;
+
+    [SerializeField] public GameObject playerCamera;
+
+    public static Rigidbody playerRb;
+
+    public static int Health;
+
+    public static int Chapter;
+
+    public static int Level;
+
+    public bool YouAreInThePresent;
+
+    public static int[] ElementInHand = new int [4];
+
+    private bool playOnce = false;
+
+    public enum Elements
+    {
+        None, Fire, Water, Air, Earth
+    }
+
+    public List<InteractBy> interactByList = new List<InteractBy>();
+
+    private void Awake()
+    {
+        instance = this;
+
+        playerController4 = FindObjectOfType<PlayerController4>(); //Find movement script
+        playerRb = playerGameObject.GetComponent<Rigidbody>(); //Find player Rigidbody
+
+        playerInteraction = FindObjectOfType<PlayerInteraction>();
+        interactiveElement = FindObjectOfType<InteractiveElement>();
+        fireOnBrazier = FindObjectOfType<FireOnBrazier>();
+        health = FindObjectOfType<Health>();
+        audioManager = FindObjectOfType<AudioManager>();
+        door2 = FindObjectOfType<Door2>();
+        damage = FindObjectOfType<Damage>();
+
+        if(YouAreInThePresent)
+        {
+            AudioManager.instance.PresentSnapshot();
+        }
+        else
+        {
+            AudioManager.instance.PastSnapshot();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactByList != null && Input.GetKeyDown(KeyCode.E))
+        {
+            foreach(InteractBy interactBy in interactByList)
+            {
+                interactBy.Interact();
+            }            
+        }
+
+        if(health.health == 0)
+        {
+            playerController4.gameObject.SetActive(false);
+            FakePlayer.SetActive(true);
+            FakePlayer.transform.position = playerController4.gameObject.transform.position;
+        }
+
+        StartCoroutine((PlayerDeath()));
+    }
+    public IEnumerator WaitASec(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait); // serve per creare il tempo giusto prima di far partire la scena della morte con una quantità di tempo
+    }
+
+    IEnumerator PlayerDeath()
+    {
+        if (health.health == 0 && !playOnce)
+        {
+            playOnce = true;
+            AudioManager.instance.PlayDeath();
+            playerController4.gameObject.SetActive(false);
+            FakePlayer.SetActive(true);
+            FakePlayer.transform.position = playerController4.gameObject.transform.position;
+            yield return StartCoroutine(WaitASec(3));
+            SceneManager.LoadScene(4);
+            PlayerPrefs.SetInt("health",3);
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] bool isRunning;
+    private Animator playerAn;
+
+    public float maxFallDistance = 0f;
+
+    public Transform tr;
+    public Transform spawnPointPresent;
+    public Transform spawnPointPast;
+
+    public bool ignoreInput = false;
+
+    [SerializeField] Transform groudPosition;
+    [SerializeField] float rangeGroundCheck = 0.1f;
+    [SerializeField] float heightGroundCheck = 0.4f;
+    [SerializeField] bool showGroundCheck;
+
+    GameObject lastPlatform;
+
+    Rigidbody rb;
+
+    [SerializeField] Collider colliderFriction;
+
+    [SerializeField] LayerMask groundcheckmask;
+    bool lastframeOnIce = false;
+    //public float maxVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2.5f;
+        isRunning = false;
+        maxFallDistance = spawnPointPresent.position.y - 10f;
+        maxFallDistance = spawnPointPast.position.y - 10f;
+        rb = GetComponent<Rigidbody>();
+        playerAn = GetComponent<Animator>(); //aggiunta elisa
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!ignoreInput)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.0f || Input.GetAxisRaw("Horizontal") < -0.0f
+                || Input.GetAxisRaw("Vertical") > 0.0f || Input.GetAxisRaw("Vertical") < -0.0f)
+            {
+                isRunning = false;
+
+                float assX = Input.GetAxis("Horizontal");
+
+                float assZ = Input.GetAxis("Vertical");
+
+                Vector3 direction = new Vector3(assX, 0, assZ).normalized;
+
+                transform.rotation = Quaternion.LookRotation(direction);
+
+                rb.velocity = new Vector3(direction.x * speed, rb.velocity.y, direction.z * speed);
+
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) && isRunning == false)
+                {
+                    isRunning = true;
+                    speed = 3.5f;
+                }
+                if (!isRunning)
+                {
+                    speed = 2.5f;
+                }
+
+                /*if(rb.velocity.magnitude > maxVelocity)
+                {
+                    rb.velocity = rb.velocity.normalized * maxVelocity;
+                }
+                else
+                {
+                    rb.AddForce(direction * acceleration * Time.fixedDeltaTime);
+                }*/
+
+                playerAn.SetBool("is_walking", true); //aggiunta elisa
+            }
+            else
+            {
+                playerAn.SetBool("is_walking", false); //aggiunta elisa
+            }
+
+            //rb.AddForce(direction * speed);
+        }
+        else
+        {
+            playerAn.SetBool("is_walking", false); //aggiunta elisa
+        }
+
+        Collider[] colliders = Physics.OverlapBox(groudPosition.position, new Vector3(rangeGroundCheck, heightGroundCheck, rangeGroundCheck),Quaternion.identity,groundcheckmask);
+        GameObject tempPlatform = null;
+        bool lastPlatformPresent = false;
+        bool OnlyOnIce = true;
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag != "colliderIce")
+            {
+                OnlyOnIce = false;
+            }
+            if (collider.tag == "Platform")
+            {
+                tempPlatform = collider.gameObject;
+                if (lastPlatform != null && tempPlatform == lastPlatform)
+                {
+                    lastPlatformPresent = true;
+                    break;
+                }
+            }
+        }
+
+        if (OnlyOnIce)
+        {
+            OnIce();       
+        }
+        else
+        {
+            if (lastframeOnIce)
+            {
+                EndIgnoreInput();
+            }
+        }
+
+        if (!lastPlatformPresent)
+        {
+            lastPlatform = tempPlatform;
+        }
+        if (lastPlatform != null)
+        {
+            transform.parent = lastPlatform.transform;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
+        if (tr.position.y < maxFallDistance && GameManager.instance.YouAreInThePresent == true && GameManager.health.health > 0)
+        {
+            GameManager.health.DecreaseHealth();
+            GameManager.instance.playerGameObject.transform.position = spawnPointPresent.transform.position;
+        }
+        if (tr.position.y < maxFallDistance && GameManager.instance.YouAreInThePresent == false && GameManager.health.health > 0)
+        {
+            GameManager.health.DecreaseHealth();
+            GameManager.instance.playerGameObject.transform.position = spawnPointPast.transform.position;
+        }
+        if (GameManager.health.health == 1)
+        {
+            spawnPointPresent = null;
+            spawnPointPast = null;
+        }
+
+        lastframeOnIce = OnlyOnIce;
+
+    }
+    private void OnDrawGizmos()
+    {
+        if (showGroundCheck)
+        {
+            Gizmos.DrawCube(groudPosition.position, new Vector3(rangeGroundCheck, heightGroundCheck, rangeGroundCheck));
+        }
+    }
+    public void StartIgnoreInput()
+    {
+        ignoreInput = true;
+    }
+
+    public void EndIgnoreInput()
+    {
+        ignoreInput = false;
+    }
+
+    public IEnumerator EndIgnoreInputAfterCoroutine(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        GameManager.playerController4.EndIgnoreInput();
+    }
+
+    void OnIce()
+    {
+        {
+            if (!ignoreInput)
+            {
+                if (Input.GetAxisRaw("Horizontal") > 0.0f || Input.GetAxisRaw("Horizontal") < -0.0f
+               || Input.GetAxisRaw("Vertical") > 0.0f || Input.GetAxisRaw("Vertical") < -0.0f)
+
+                {
+                    isRunning = false;
+
+                    Debug.Log("Inizio");
+                    StartIgnoreInput();
+
+                    float assX = Input.GetAxis("Horizontal");
+
+                    float assZ = Input.GetAxis("Vertical");
+
+                    Vector3 direction = new Vector3(assX, 0, assZ).normalized;
+
+                    transform.rotation = Quaternion.LookRotation(direction);
+
+                    //rb.AddForce(direction * speed);
+
+                    rb.velocity = direction * 3.5f;
+
+                    colliderFriction.material.dynamicFriction = 0;
+                    colliderFriction.material.staticFriction = 0;
+                }
+            }
+        }
+
+    }  
+}
